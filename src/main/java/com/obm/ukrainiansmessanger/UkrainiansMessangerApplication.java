ef6b5f6e80928f1908072d8ba0cc88ab53874ff8
Aleksander <zagorenkosahsa@gmail.com>
2022-07-12 17:39:30 +0300

    Create Project

---
+++ src/main/java/com/obm/ukrainiansmessanger/UkrainiansMessangerApplication.java
@@ -0,0 +1,13 @@
+package com.obm.ukrainiansmessanger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UkrainiansMessangerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UkrainiansMessangerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/obm/ukrainiansmessanger/UkrainiansMessangerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.obm.ukrainiansmessanger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UkrainiansMessangerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
