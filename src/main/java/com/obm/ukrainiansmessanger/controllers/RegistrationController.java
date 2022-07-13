5f32d288a0f1ce071326ba6e5b25b1671ce75848
Дмитрий <dimabukov52@gmail.com>
2022-07-13 07:03:56 +0300

    Add registration(maybe)

--- src/main/java/com/obm/ukrainiansmessanger/controllers/RegistrationController.java
+++ src/main/java/com/obm/ukrainiansmessanger/controllers/RegistrationController.java
@@ -1,7 +1,30 @@
 package com.obm.ukrainiansmessanger.controllers;
 
+import com.obm.ukrainiansmessanger.models.User;
+import com.obm.ukrainiansmessanger.repos.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Map;
 
 @Controller
 public class RegistrationController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/registration")
+    public String registration(){
+        return "registraion";
+    }
+    @PostMapping("/registration")
+    public String addUser(User user, Map<String, Object> model){
+       User userFromDb = userRepository.findByUsername(user.getUsername());
+       if(userFromDb != null){
+           model.put("message", "Такий користувач вже існує");
+           return "registration";
+       }
+        return "redirect:/login";
+    }
 }
