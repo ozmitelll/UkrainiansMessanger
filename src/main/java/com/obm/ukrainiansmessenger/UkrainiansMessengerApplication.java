a44767442807db058d42ede46555e903563a2931
Aleksander <zagorenkosahsa@gmail.com>
2022-07-13 16:06:55 +0300

    Settings Security

---
+++ src/main/java/com/obm/ukrainiansmessenger/UkrainiansMessengerApplication.java
@@ -0,0 +1,13 @@
+package com.obm.ukrainiansmessenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UkrainiansMessengerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UkrainiansMessengerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/obm/ukrainiansmessenger/UkrainiansMessengerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.obm.ukrainiansmessenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UkrainiansMessengerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
